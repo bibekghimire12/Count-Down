f1124cc21be558d336436fad9c9d10185bd61dd1
bibekghimire12 <ghimirebibek620@gmail.com>
2024-05-15 21:06:34 +0545

    sample commit

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+const endDate = "03 March 2025 9:36 PM";
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+function clock(){
+  const now = new Date();
+  const end = new Date(endDate)
+  const diff = (end - now)/1000;
+  if(diff < 0) return;
+  inputs[0].value =  Math.floor(diff/86400)
+  inputs[1].value = (Math.floor(diff/3600) % 24)
+  inputs[2].value = (Math.floor(diff/60) % 60)
+  inputs[3].value = (Math.floor(diff) % 60)
+}
+clock();
+
+setInterval(
+  ()=>{
+    clock()
+  }, 1000
+)
